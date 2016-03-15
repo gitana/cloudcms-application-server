f606159e7c35f47ee6e7277c7a506cab108c3922
Harry Moore <harry@Harrys-MAC.home>
2016-03-15 13:47:13 -0400

    simplify app.js

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
 var server = require("cloudcms-server/server");
 
-var frameworkControllers = require("cloudcms-server/framework/controllers");
-var frameworkSockets = require("cloudcms-server/framework/sockets");
-
-// use the special "net-production" configuration
-if (!process.env.CLOUDCMS_STORE_CONFIGURATION) {
-    //process.env.CLOUDCMS_STORE_CONFIGURATION = "net-production";
-    
-    // TODO: for now, fall back to net-development since we rely on the /hosts directory
-    process.env.CLOUDCMS_STORE_CONFIGURATION = "net-development";
-}
-
-/**
- * Default route handlers
- */
-server.routes(function(app, callback) {
-    frameworkControllers.init(app, function(err) {
-        callback(err);
-    });
-});
-
-/**
- * Default socket handlers
- */
-server.sockets(function(socket, callback) {
-    frameworkSockets.init(socket, function(err) {
-        callback(err);
-    });
-});
-
 // after
 server.after(function(app, callback) {
     callback();
@@ -36,13 +7,13 @@ server.after(function(app, callback) {
 
 // report
 server.report(function(callback) {
-    
+
     console.log("");
     console.log("Cloud CMS Presentation Server started! (v" + process.env.CLOUDCMS_APPSERVER_PACKAGE_VERSION + ")");
     console.log("");
-            
+
     var cpuCount = require('os').cpus().length;
-    
+
     // provide some debug info
     console.log("");
     console.log("Node Version: " + process.version);
@@ -58,34 +29,25 @@ server.report(function(callback) {
     console.log("Cache Provider: " + process.env.CLOUDCMS_CACHE_TYPE);
     console.log("Temp Directory: " + process.env.CLOUDCMS_TEMPDIR_PATH);
     console.log("LaunchPad Mode: " + process.env.CLOUDCMS_LAUNCHPAD_SETUP);
-    
-    console.log("");        
+
+    console.log("");
     console.log("Web Server: http://localhost:" + process.env.PORT);
-    console.log("");        
-    
+    console.log("");
+
     callback();
-    
+
 });
 
 // start the server
 var config = {
     "setup": "single",
     "virtualHost": {
-        "enabled": true
+        "enabled": false
     },
     "wcm": {
         "enabled": true,
         "cache": false
-    },
-    "serverTags": {
-        "enabled": true
-    },
-    "autoRefresh": {
-        "log": true
-    },
-    "insight": {
-        "enabled": true
-    }            
+    }
 };
 
 server.start(config);
